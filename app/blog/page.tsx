051372104f1fc35895f201182a0c0262664e018d
malewis5 <matthew.a.lewis7@gmail.com>
2025-07-16 04:18:50 +0000

    Add new blog page

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold mb-4 text-black">Blog</h1>
+      <p className="text-lg text-gray-600">Welcome to the blog page. Stay tuned for updates!</p>
+    </main>
+  );
+}
